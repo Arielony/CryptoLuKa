87e6914e2afd0226314b606e2e7b858504487af8
Arielony <ariel_saher@hotmail.com>
2018-10-09 15:16:53 +0000

    MyCoin

--- cryptonote/src/CryptoNoteCore/Currency.cpp
+++ cryptonote/src/CryptoNoteCore/Currency.cpp
@@ -99,7 +99,7 @@ bool Currency::generateGenesisBlock() {
   //std::string hex_tx_represent = Common::toHex(txb);
 
   // Hard code coinbase tx in genesis block, because through generating tx use random, but genesis should be always the same
-  std::string genesisCoinbaseTxHex = "010a01ff00018080d287e2bc2d02400fa471f42b5d2619835d5387609df104b3af197de62c638ae0d518d9d06b3a2101cf744b38fb9843560d7bff26c5b25350bdb4c5fdde62a63b62aea829cc643491";
+  std::string genesisCoinbaseTxHex = "010a01ff000180c0caf384a302024c9afd406ac3047581d874eb2a1a88a50b6ea253bb38509b550e1e47dfa080732101f23297c85a0d0b84c2c1dd8a0b17c408e8aca6dc55d5ca1654b78138469aa887";
   BinaryArray minerTxBlob;
 
   bool r =
@@ -174,20 +174,10 @@ bool Currency::getBlockReward(uint8_t blockMajorVersion, size_t medianSize, size
 
   // LUK REWARD CHANGES
   uint64_t baseReward;
-  uint64_t factorCut = 44500000; // 0.445 LUK
-  uint64_t coinCut_v1 = 264000000000000; // 2.64M LUK
-  uint64_t coinCut_v2 = 292000000000000; // 2.92M LUK
+ 
+ // 100M >> 22
+  baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
 
-  if(alreadyGeneratedCoins < coinCut_v1) {
-    // 100M >> 22
-    baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
- } else if (alreadyGeneratedCoins >= coinCut_v1 && alreadyGeneratedCoins <= coinCut_v2) {
-    // 100M -> 0,445
-    baseReward = factorCut;
- } else {
-    // 100M >> 22
-    baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
- }
 
 
   //infinite minimal block rewards after block reward falls under m_finalSubsidy per minute
